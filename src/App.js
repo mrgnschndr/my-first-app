41f8027e7d4c79a80cbfd72bba6e51162c3e480a
mrgnschndr <mrgnschndr@gmail.com>
2024-11-04 23:43:32 -0700

    wip: main post image and interaction icons near bottom close to pixel-perfect

--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@ export default class App extends React.Component {
   render() {
     return(
       <div>
-        <p>Proof Of Life</p>
         <Post />
       </div>
     );
--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -1,27 +1,42 @@
 import React, { Component } from 'react';
+// CSS file
+import './Post.css';
+// from FA docs
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHeart } from '@fortawesome/fontawesome-free-regular';
+import { faComment } from '@fortawesome/fontawesome-free-regular';
+import { faPaperPlane } from '@fortawesome/fontawesome-free-regular';
+import { faBookmark } from '@fortawesome/fontawesome-free-regular';
+import { faEllipsis } from '@fortawesome/free-solid-svg-icons';
+
+
 
 class Post extends Component {
     render() {
         return(
-        <div>
+        <div id="post-wrap">
             <div>
-                <img></img>
+                {/* Add 32x32 profile image to match instagram.com mobile view */}
+                <img src="https://placehold.co/32"></img>
                 <div>
-                    <h3>Username</h3>
-                    <h4>Location</h4>
+                    <h3>username</h3>
+                    <h4>location</h4>
                 </div>
-                <button>Follow</button>
-                <i></i>
+                <FontAwesomeIcon icon={faEllipsis} class="interact" />
             </div>
-            <img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.instagram.com%2Fbeckfeiner_illustrations%2Fp%2FC5xcJ5jPcOv%2F&psig=AOvVaw1DDad5rDR5W_az1qXxThZh&ust=1730866845918000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCNjptdOrxIkDFQAAAAAdAAAAABAE" alt="Placeholder image for a post component. Currently an image of a dog and magpie with text 'Peggy and Molly Reunited'."></img>
+            <img src="https://placehold.co/400" alt="Placeholder image" />
             <div>
-                <i></i>
-                <p></p>
-                <i></i>
-                <p></p>
-                <i></i>
-                <p></p>
-                <i></i>
+                <div>
+                    <FontAwesomeIcon icon={faHeart} class="interact" />
+                    {/* <p></p> */}
+                    <FontAwesomeIcon icon={faComment} class="interact" />
+                    {/* <p></p> */}
+                    <FontAwesomeIcon icon={faPaperPlane} class="interact" />
+                    {/* <p></p> */}
+                </div>
+                <div>
+                    <FontAwesomeIcon icon={faBookmark} class="interact" />
+                </div>
             </div>
         </div>  
         )
