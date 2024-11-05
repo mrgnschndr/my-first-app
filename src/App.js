ab9e99babf33ea34db5cf468ee89753a91d8a185
mrgnschndr <mrgnschndr@gmail.com>
2024-11-04 21:09:24 -0700

    wip: finish boilerplate imports/exports, and finish jsx structure for Post

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Post from './Post.js';
 
 export default class Logo extends React.Component {
   render() {
---
+++ src/components/Post/Post.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+
+class Post extends Component {
+    render() {
+        return(
+        <div>
+            <div>
+                <img></img>
+                <div>
+                    <p>Username</p>
+                    <p>Location</p>
+                </div>
+                <button>Follow</button>
+                <i></i>
+            </div>
+            <img></img>
+            <div>
+                <i></i>
+                <p></p>
+                <i></i>
+                <p></p>
+                <i></i>
+                <p></p>
+                <i></i>
+            </div>
+        </div>  
+        )
+    }
+};
+
+
+
+
+
+export default Post;
