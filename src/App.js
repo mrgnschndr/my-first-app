f7d8d1584fca8d42887ce22dc6fcbe44e46528fa
mrgnschndr <mrgnschndr@gmail.com>
2024-11-04 21:36:38 -0700

    wip: App with Post is working in web prevous

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
 import React, { Component } from 'react';
-import Post from './Post.js';
+import Post from '../src/components/Post/Post.js';
+// Post.css may not belong in this file...
+// import '../src/components/Post/Post.css';
 
-export default class Logo extends React.Component {
+export default class App extends React.Component {
   render() {
-    return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}}
+    return(
+      <div>
+        <p>Proof Of Life</p>
+        <Post />
+      </div>
+    );
+  }
+}
+
+
+// Below is from previous homework
+// export default class Logo extends React.Component {
+//   render() {
+//     return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={Logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }}
+
 
 // export default Logo;
--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -7,13 +7,13 @@ class Post extends Component {
             <div>
                 <img></img>
                 <div>
-                    <p>Username</p>
-                    <p>Location</p>
+                    <h3>Username</h3>
+                    <h4>Location</h4>
                 </div>
                 <button>Follow</button>
                 <i></i>
             </div>
-            <img></img>
+            <img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.instagram.com%2Fbeckfeiner_illustrations%2Fp%2FC5xcJ5jPcOv%2F&psig=AOvVaw1DDad5rDR5W_az1qXxThZh&ust=1730866845918000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCNjptdOrxIkDFQAAAAAdAAAAABAE" alt="Placeholder image for a post component. Currently an image of a dog and magpie with text 'Peggy and Molly Reunited'."></img>
             <div>
                 <i></i>
                 <p></p>
@@ -27,7 +27,6 @@ class Post extends Component {
         )
     }
 };
-
 
 
 
